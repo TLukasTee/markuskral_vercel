b35971c444bea8ef2ec2961f1ff70f48613affec
egor <egorpod.ep@gmail.com>
2024-06-01 20:19:36 +0200

    hardcode

--- src/app/_components/Media/Image/index.tsx
+++ src/app/_components/Media/Image/index.tsx
@@ -43,7 +43,7 @@ export const Image: React.FC<MediaProps> = props => {
 
     const filename = fullFilename
 
-    src = `${process.env.NEXT_PUBLIC_SERVER_URL}/media/${filename}`
+    src = `https://markuskral-production.up.railway.app/media/${filename}`
     console.log(process.env.NEXT_PUBLIC_SERVER_URL)
   }
 
