7678302f94484a9f672e354c10ca69184893fcdb
egor <egorpod.ep@gmail.com>
2024-06-01 19:12:25 +0200

    hardcoding everything up in this b

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost",process.env.NEXT_PUBLIC_SERVER_URL]
+    domains: ["localhost",'markuskral-production.up.railway.app']
       .filter(Boolean)
       .map(url => url.replace(/https?:\/\//, '')),
   },
