c6fdff5f5c8886ea5cb95838ef00e8e4e25764c3
Lukaste <123906068+TLukasTee@users.noreply.github.com>
2024-08-06 19:40:37 +0200

    Current Fix

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -89,59 +89,6 @@ export  function Header() {
               ÜBERSICHT
             </a>
 
-          <Popover className="relative">
-        
-           
-            <Popover.Button className={`flex items-center gap-x-1 ${tub.className}  text-md font-bold leading-6 text-gray-900 outline-none border-none tracking-widest`}>
-            SCHAURAUM
-              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-            </Popover.Button>
-   
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="opacity-0 translate-y-1"
-              enterTo="opacity-100 translate-y-0"
-              leave="transition ease-in duration-150"
-              leaveFrom="opacity-100 translate-y-0"
-              leaveTo="opacity-0 translate-y-1"
-            >
-              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-                <div className="p-4">
-                  {products.map((item) => (
-                    <div
-                      key={item.name}
-                      className="group relative flex gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
-                    >
-                      <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-red-600" aria-hidden="true" />
-                      </div>
-                      <div className="flex-auto">
-                        <a href={item.href} title="Markus Kral Antiquitäten | Menü" className= {`${tub.className} block font-bold  text-lg  text-gray-900`}>
-                          {item.name}
-                          <span className="absolute inset-0" />
-                        </a>
-                        <p className="mt-1 text-gray-600">{item.description}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className="grid grid-cols-1 divide-x divide-gray-900/5 bg-red-500">
-              
-                    <a title="Markus Kral Antiquitäten | Schauraum"
-                        href="/schauraum"
-                      className={`relative text-red-500    ${tub.className} text-base font-semibold  flex items-center justify-center gap-x-2.5 p-3 text-md  leading-6 text-white tracking-widest uppercase  hover:bg-red-600`}
-                    >
-                    
-                      Gesamtübersicht
-                    </a>
-              
-                </div>
-              </Popover.Panel>
-            </Transition>
-          </Popover>
-
-         
           <a href="/wertermittlung" title="Markus Kral Antiquitäten | Wertermittlung" className={`flex items-center gap-x-1 ${tub.className}  text-md font-bold leading-6 text-gray-900 outline-none border-none tracking-widest`}>
             WERTERMITTLUNG
           </a>
